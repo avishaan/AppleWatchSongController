7088f517fb371d9c5fb15b799eb2fde49e0cefc6
Avishaan <avishaan@codeHatcher.com>
2015-05-25 20:44:20 -0700

    initial iPhone controlling app layout and watchkit setup

---
+++ SongControl WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  SongControl WatchKit Extension
+//
+//  Created by Brown Magic on 5/25/15.
+//  Copyright (c) 2015 codeHatcher. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
