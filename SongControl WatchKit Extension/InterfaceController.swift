e5ca1d142860d4fb1b9f48a882d122fd5fcc5332
Avishaan <avishaan@codeHatcher.com>
2015-05-29 22:08:11 -0700

    add UI elements to watch and hook up buttons

--- SongControl WatchKit Extension/InterfaceController.swift
+++ SongControl WatchKit Extension/InterfaceController.swift
@@ -11,21 +11,34 @@ import Foundation
 
 
 class InterfaceController: WKInterfaceController {
-
-    override func awakeWithContext(context: AnyObject?) {
-        super.awakeWithContext(context)
-        
-        // Configure interface objects here.
-    }
-
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
+  
+  @IBOutlet weak var songTitleLabel: WKInterfaceLabel!
+  override func awakeWithContext(context: AnyObject?) {
+    super.awakeWithContext(context)
+    
+    // Configure interface objects here.
+  }
+  
+  override func willActivate() {
+    // This method is called when watch view controller is about to be visible to user
+    super.willActivate()
+  }
+  
+  override func didDeactivate() {
+    // This method is called when watch view controller is no longer visible
+    super.didDeactivate()
+  }
+  
+  @IBAction func previousSongButtonPressed() {
+    
+  }
+  
+  @IBAction func nextSongButtonPressed() {
+    
+  }
+  
+  @IBAction func playSongButtonPressed() {
+    
+  }
 
 }
