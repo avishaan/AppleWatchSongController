e77852adebc122024db19ae147799a7af44f9b3d
Avishaan <avishaan@codeHatcher.com>
2015-05-29 22:33:30 -0700

    simple communication with iPhone app

--- SongControl/AppDelegate.swift
+++ SongControl/AppDelegate.swift
@@ -40,6 +40,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func applicationWillTerminate(application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
   }
+  
+  //MARK: - WatchKit Extension
+  
+  func application(application: UIApplication, handleWatchKitExtensionRequest userInfo: [NSObject : AnyObject]?, reply: (([NSObject : AnyObject]!) -> Void)!) {
+    reply(["Working": "Now"])
+  }
 
 
 }
